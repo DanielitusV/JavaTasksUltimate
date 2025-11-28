e1b1b2830e10ea69c46cd2d84272548bafbd2fc3
Daniel_Virreira <virreira.daniel@gmail.com>
2025-11-27 22:06:38 -0400

    Finalizar clase Main

--- src/main/java/com/javatasksultimate/Main.java
+++ src/main/java/com/javatasksultimate/Main.java
@@ -1,7 +1,17 @@
-package main.java.com.javatasksultimate;
+package com.javatasksultimate;
+
+import com.javatasksultimate.access_data.FileTaskDAO;
+import com.javatasksultimate.access_data.TaskDAO;
+import com.javatasksultimate.logic_service.TaskService;
+import com.javatasksultimate.logic_service.TaskServiceImpl;
+import com.javatasksultimate.presentation.ConsoleUI;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        TaskDAO taskDAO = new FileTaskDAO();
+        TaskService taskService = new TaskServiceImpl(taskDAO);
+        ConsoleUI consoleUI = new ConsoleUI(taskService);
+
+        consoleUI.start();
     }
 }
